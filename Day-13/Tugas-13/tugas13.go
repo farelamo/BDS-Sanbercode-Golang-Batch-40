d3cfdc7930ea5ea5b5f7e69e24e9b47169367328
farelamo <farizfahmi674@gmail.com>
2022-12-08 14:31:03 +0700

    Finish Tugas 13

--- Day-13/Tugas-13/tugas13.go
+++ Day-13/Tugas-13/tugas13.go
@@ -17,13 +17,6 @@ type NilaiMahasiswa struct {
 
 var nilaiNilaiMahasiswa = []NilaiMahasiswa{}
 
-func DataNilaiMahasiswa() []NilaiMahasiswa {
-	dataNilai := []NilaiMahasiswa{
-		{ID: 1, Nama: "Nindy Clarastika", MataKuliah: "Matematika", IndeksNilai: "A", Nilai: 100},
-	}
-	return dataNilai
-}
-
 // Auth Middleware
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
@@ -43,29 +36,11 @@ func Auth(next http.Handler) http.Handler {
 	})
 }
 
-// Convert Nilai Middleware
-func convertNilai(next http.Handler) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
-		var tmp NilaiMahasiswa
-		decodeJSON 	:= json.NewDecoder((r.Body))
-		if err 	    := decodeJSON.Decode(&tmp); err != nil {
-			log.Fatal(err)
-		}
-
-		if tmp.Nilai >= 80 {
-			tmp.IndeksNilai = "A"
-		}else if(tmp.Nilai >= 80){
-			// Abis ini dilanjut bang, ini aku push dulu kwkw
-		}
-	})
-}
-
 // GET
 func getNilaiMahasiswa(w http.ResponseWriter, r *http.Request){
 	if r.Method == "GET" {
-		data  := DataNilaiMahasiswa()
 
-		encodeData, err := json.Marshal(data)
+		encodeData, err := json.Marshal(&nilaiNilaiMahasiswa)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -82,28 +57,45 @@ func getNilaiMahasiswa(w http.ResponseWriter, r *http.Request){
 // POST
 func addNilaiMahasiswa(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
-	var nilaiNilaiMahasiswa NilaiMahasiswa
+	var tmp NilaiMahasiswa
 	if r.Method == "POST" {
 		if r.Header.Get("Content-Type") == "application/json" {
 			decodeJSON 	:= json.NewDecoder((r.Body))
-			if err 	    := decodeJSON.Decode(&nilaiNilaiMahasiswa); err != nil {
+			if err 	    := decodeJSON.Decode(&tmp); err != nil {
 				log.Fatal(err)
 			}
 		}else {
 			http.Error(w, "Data harus berupa JSON", http.StatusBadRequest)
+			return
 		}
 		
-		dataMahasiswa, _ := json.Marshal(nilaiNilaiMahasiswa)
-		w.Write(dataMahasiswa)
+		if tmp.Nilai >= 80 {
+			tmp.IndeksNilai = "A"
+		}else if(tmp.Nilai < 80 && tmp.Nilai >= 70){
+			tmp.IndeksNilai = "B"
+		}else if(tmp.Nilai < 70 && tmp.Nilai >= 60){
+			tmp.IndeksNilai = "C"
+		}else if(tmp.Nilai < 60 && tmp.Nilai >= 50){
+			tmp.IndeksNilai = "D"
+		}else {
+			tmp.IndeksNilai = "E"
+		}
+
+		tmp.ID = uint(len(nilaiNilaiMahasiswa) + 1)
+
+		nilaiNilaiMahasiswa = append(nilaiNilaiMahasiswa, tmp)
+		result, _ 			:= json.Marshal(nilaiNilaiMahasiswa)
+		w.Write(result)
 		return
 	}
+
 	http.Error(w, "Method Salah", http.StatusMethodNotAllowed)
 }
 
 func main(){
 	// Route
 	http.HandleFunc("/", getNilaiMahasiswa)
-	http.Handle("/create", Auth(convertNilai(http.HandlerFunc(addNilaiMahasiswa))))
+	http.Handle("/create", Auth(http.HandlerFunc(addNilaiMahasiswa)))
 
 	fmt.Println("Server running at http://localhost:8000")
 
